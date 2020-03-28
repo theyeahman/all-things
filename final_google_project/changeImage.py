b6e3a53c147a80aaa1ccdaedcfdb327e2d0a968e
thomas yeaman <yeamanthomas@gmail.com>
2020-03-28 10:20:44 +0000

    created file skeletons for the first two steps of google challenge

---
+++ final_google_project/changeImage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+# task 1: user enters relative location of images, we locate
+# absolute path of images
+
+
+# task 2: create list of images from directory (list dir and if tiff)
+
+# task 3  convert all images 600x400 pixel and jpg
+# save them back to  same directory
---
+++ final_google_project/supplier_image_upload.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import requests
+
+# uploads to directory of new images to web server catalog
