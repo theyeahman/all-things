b4e80effa1f56ca2dced80659ea1ef3979c1e62d
thomas yeaman <yeamanthomas@gmail.com>
2020-03-30 14:00:34 +0100

    added image_name to dictionary and included post step

--- final_google_project/upload_info.py
+++ final_google_project/upload_info.py
@@ -1,28 +1,31 @@
-
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 import os
-import requests
-import json
 
 
-def open_file(file):
+
+def open_file(directory,file):
     dict_text = {}
     keys = ["name","weight","description"]
     count = 0
-    with open(file) as f:
+    with open(os.path.join(directory,file)) as f:
         for line in f:
             dict_text[keys[count]] = line.strip()
             if keys[count] == 'weight':
                 dict_text[keys[count]] = line[:-5]
             count += 1
-    print(dict_text)
+        dict_text['image_name'] = file[:-4]+'.jpeg'
+
     return dict_text
 
 def main():
     dir = "./supplier-data/descriptions/"
+    dir_img = "./supplier-data/images/"
+    url = "http://[linux-instance-external-IP]/fruits"
     for i in os.listdir(dir):
         if i[-3:] == "txt":
-            comp_dict =  open_file(os.path.join(dir,i))
+            comp_dict =  open_file(dir,i)
+            print(comp_dict)
+            requests.post("http://[linux-instance-external-IP]/fruits", data=comp_dict)
 
 if __name__ == '__main__':
     main()
